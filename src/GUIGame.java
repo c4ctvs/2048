848354fe3f097263fbcd7da60d2cc4f23754a060
Zuzanna Kopec <zuzanna.kopec12@gmail.com>
2021-04-27 09:57:45 +0200

    Graphic representation

---
+++ src/GUIGame.java
@@ -0,0 +1,134 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class GUIGame extends Engine implements Game{
+    GUI gui;
+    public void Play(){
+        Show();
+    }
+    public void Show(){
+        gui = new GUI();
+    }
+
+public class GUI extends JFrame{
+    int spacing = 5;
+    GUI(){
+        this.setTitle("2048");
+        this.setSize(600, 600);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Board boardG = new Board();
+        this.setContentPane(boardG);
+        Arrows arrows = new Arrows();
+        this.addKeyListener(arrows);
+    }
+
+
+
+    public class Board extends JPanel {
+        public void paintComponent(Graphics g) {
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, 0, 600, 600);
+            g.setColor(Color.decode("#EDC691"));
+            g.setFont(new Font("Calibri", Font.BOLD, 30));
+
+            for (int x = 0; x < 4; x++) {
+                for (int y = 0; y < 4; y++) {
+                    if(board[x][y].getValue() == 0)
+                        {
+                             fillColor(g,"#EDC691", x,y);
+                        }
+                    else if(board[x][y].getValue() == 2)
+                         {
+                             fillColor(g,"#EBBA78", x,y);
+                         }
+                    else if(board[x][y].getValue() == 4)
+                         {
+                             fillColor(g,"#EBAA52", x,y);
+                         }
+                    else if(board[x][y].getValue() == 8)
+                         {
+                             fillColor(g,"#E89f3C", x,y);
+                         }
+                    else if(board[x][y].getValue() == 16)
+                         {
+                             fillColor(g,"#E69155", x,y);
+                         }
+                    else if(board[x][y].getValue() == 32)
+                         {
+                             fillColor(g,"#E8823A", x,y);
+                         }
+                    else if(board[x][y].getValue() == 64)
+                         {
+                             fillColor(g,"#FFBF47", x,y);
+                         }
+                    else if(board[x][y].getValue() == 128)
+                    {
+                        fillColor(g,"#ffb324", x,y);
+                    }
+                    else if(board[x][y].getValue() == 256)
+                    {
+                        fillColor(g,"#ffb04f", x,y);
+                    }
+                    else if(board[x][y].getValue() == 512)
+                    {
+                        fillColor(g,"#ff8e3d", x,y);
+                    }
+                    else if(board[x][y].getValue() == 1024)
+                    {
+                        fillColor(g,"#ffd04f", x,y);
+                    }
+                    else if(board[x][y].getValue() == 2048)
+                    {
+                        fillColor(g,"#fcba05", x,y);
+                    }
+                    if(board[x][y].getValue() != 0)
+                    {
+                        g.setColor(Color.BLACK);
+                        g.drawString(Integer.toString(board[x][y].getValue()), x*80 + 140, y*80 + 80 +80);
+                    }
+
+
+                }
+            }
+            repaint();
+        }
+
+        void fillColor(Graphics g, String color, int x, int y)
+        {
+            g.setColor(Color.decode(color));
+            g.fillRect((spacing + 3 * x) * 25, (spacing + 3 * y) * 25, 80 - 2 * spacing, 80 - 2 * spacing);
+        }
+
+    }
+}
+
+
+public class Arrows implements KeyListener {
+
+        @Override
+        public void keyTyped(KeyEvent e) {}
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            switch (key) {
+                case KeyEvent.VK_LEFT -> moveUp();
+                case KeyEvent.VK_RIGHT -> moveDown();
+                case KeyEvent.VK_UP -> moveLeft();
+                case KeyEvent.VK_DOWN -> moveRight();
+            }
+            generateRandomSquares();
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {}
+    }
+}
